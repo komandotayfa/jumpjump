6dac5893e60f84bbf424dcbc6183953487f9342e
komandotayfa <53448093+komandotayfa@users.noreply.github.com>
2024-05-31 17:54:47 +0300

    Updated export settings

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,10 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("Dead")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
